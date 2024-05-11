7a5fc549e050aa3c0f41b2ac8469a327e2d6815b
IbragimovRr <92021334+IbragimovRr@users.noreply.github.com>
2024-05-11 19:56:35 +0300

    HomeStart2

--- Intop/View/Border.swift
+++ Intop/View/Border.swift
@@ -9,10 +9,26 @@ import Foundation
 import UIKit
 
 
-class Border {
+class Border: UIView {
+    
+    @IBInspectable var border: CGFloat = 1 {
+        didSet {
+            layer.borderWidth = border
+        }
+    }
     
     func addViewBorder(view: UIView) {
         view.layer.borderWidth = 1
         view.layer.borderColor = UIColor(named: "BorderColor")?.cgColor
+    }
+    
+    override init(frame:CGRect) {
+        super.init(frame: frame)
+        addViewBorder(view: self)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        addViewBorder(view: self)
     }
 }
