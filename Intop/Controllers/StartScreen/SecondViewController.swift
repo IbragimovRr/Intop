4d316bfd254a50abdc03710f13824c6f86540804
Eldarik25 <Fot1380123@mail.ru>
2024-05-13 15:19:04 +0300

    6Eldar (SecondViewController)

---
+++ Intop/Controllers/StartScreen/SecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  Intop
+//
+//  Created by Ибрагимов Эльдар on 13.05.2024.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
