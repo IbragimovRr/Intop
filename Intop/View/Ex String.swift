18a2351bcb3c7b4855f1729460e16f12dfe87f20
Eldarik25 <Fot1380123@mail.ru>
2024-06-01 20:40:17 +0300

    elder Profile

---
+++ Intop/View/Ex String.swift
@@ -0,0 +1,17 @@
+//
+//  Ex String.swift
+//  Intop
+//
+//  Created by Ибрагимов Эльдар on 01.06.2024.
+//
+
+import Foundation
+import UIKit
+
+extension String {
+    func height(withConstrainedWidth width: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
+        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [.font: font], context: nil)
+        return ceil(boundingBox.height)
+    }
+}
