db8d1066a3fe5cb04c2a1c21d45f180d61c6930f
Eldarik25 <Fot1380123@mail.ru>
2024-06-23 16:37:16 +0300

    eldar fixStories

--- Intop/View/ProgressView.swift
+++ Intop/View/ProgressView.swift
@@ -8,15 +8,37 @@
 import Foundation
 import UIKit
 
+private var progressTimerKey: UInt8 = 0
+
 extension UIProgressView {
+    private var timer: Timer? {
+        get {
+            return objc_getAssociatedObject(self, &progressTimerKey) as? Timer
+        }
+        set {
+            objc_setAssociatedObject(self, &progressTimerKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+    }
     
     func animate(_ seconds: Float, finish: @escaping () -> ()) {
-        Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { timer in
+        timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { timer in
             self.progress += 0.01 / seconds
             if self.progress >= 1.0 {
                 timer.invalidate()
                 finish()
             }
         }
+    }
+    func start(_ seconds: Float) {
+        timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { timer in
+            self.progress += 0.01 / seconds
+            if self.progress >= 1.0 {
+                timer.invalidate()
+            }
+        }
+    }
+    func stop() {
+        timer?.invalidate()
+        timer = nil
     }
 }
